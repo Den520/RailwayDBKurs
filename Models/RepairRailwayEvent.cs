4419c935c74b93a0fab4115e597d6af1a4daea4d
Tariel <uzumake02@mail.ru>
2023-04-08 14:59:11 +0400

    Fixed exception for begin and end points of repair railway event

--- Models/RepairRailwayEvent.cs
+++ Models/RepairRailwayEvent.cs
@@ -4,7 +4,7 @@
     {
         public int ID { get; set; }
         public int RegionID { get; set; }
-        public double? BeginPoint { get; set; }
-        public double? EndPoint { get; set; }
+        public int? BeginPoint { get; set; }
+        public int? EndPoint { get; set; }
     }
 }
